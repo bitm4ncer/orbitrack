f7c25ad2dfbca6348e4b603ebf15b517ad561881
bitmancer <bitmancer@wndrbr.de>
2026-03-09 17:49:15 +0100

    deploy

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Orbeat
  Polyrhythmic Web Sequencer 
+
+
+Use it here: https://bitm4ncer.github.io/Orbeat
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 import { sampleListPlugin } from './src/vite-plugins/sampleListPlugin'
 
 export default defineConfig({
+  base: '/Orbeat/',
   plugins: [react(), tailwindcss(), sampleListPlugin()],
 })
